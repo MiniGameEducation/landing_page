763d96bb67a842dac297b572bf881f2a744965c0
Ability1212 <ubaidilahmahendra1234@gmail.com>
2024-05-07 11:48:31 +0700

    abizar full stack

--- php/register.php
+++ php/register.php
@@ -6,14 +6,14 @@ $password=$_POST['password'];
 $level="user";
 $kirim=$_POST['signup'];
 if($kirim){
-$query="INSERT INTO `tb_user`(`id_user`, `nama`, `email`, `password`, `level`) VALUES
-('','$name','$email','$password','$level')";
-$hasil=mysqli_query($conn,$query);
-header('Location:../html/form.html');
-}else{
-    echo "<script>
-            window.open('../html/form.html');
-            alert('Register Failed!');
-          </script>";
-}
+    $query="INSERT INTO `tb_user`(`id_user`, `nama`, `email`, `password`, `level`) VALUES
+    ('','$name','$email','$password','$level')";
+    $hasil=mysqli_query($conn,$query);
+    header('Location:../html/form.html');
+    }else{
+        echo "<script>
+                window.open('../html/form.html');
+                alert('Register Failed!');
+                </script>";
+        }
 ?>
